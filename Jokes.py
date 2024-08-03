4718d0895652a8e6e1315f506cb20404a9ff7790
Rakesh <rakesh.717.02@gmail.com>
2024-08-03 09:42:40 +0530

    Initial commit

---
+++ Jokes.py
@@ -0,0 +1,18 @@
+import pyjokes
+
+joke = pyjokes.get_joke()
+print(joke)
+
+# I have pasted some of the crazy( I liked ) jokes below
+
+# How many programmers does it take to change a lightbulb? None, they just make darkness a standard.
+
+#QAs consist of 55% water, 30% blood and 15% Jira tickets.
+
+#To understand recursion you must first understand recursion.
+
+# Is reading in the bathroom considered Multi-Tasking.
+
+# Bug? That's not a bug, that's a feature.
+
+#What did the Java code say to the C code? A: You've got no class.
